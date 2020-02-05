250a280832db2f2688e9ad4f97eed0162be261e5
armando <armando.cavalca@gmail.com>
2020-02-05 11:16:54 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Game> Game { get; set; }
+    }
---
+++ WebApplication1/Pages/Games/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication1
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Game.Add(Game);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
