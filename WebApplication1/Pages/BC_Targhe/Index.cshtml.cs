60f26c5aafd90c206d4264a3b4421bb123c06f8f
armando <armando.cavalca@gmail.com>
2020-02-07 11:52:12 +0100

    miglioramenti

--- WebApplication1/Pages/BC_Targhe/Index.cshtml.cs
+++ WebApplication1/Pages/BC_Targhe/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1
@@ -18,10 +19,26 @@ namespace WebApplication1
         }
 
         public IList<BC_Targhe> BC_Targhe { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Targhe { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string targhe { get; set; }
 
         public async Task OnGetAsync()
         {
-            BC_Targhe = await _context.BC_Targhe.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.BC_Targhe
+                                            orderby m.bc_targa
+                                            select m.bc_targa;
+            var bc_targhe = from m in _context.BC_Targhe
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                bc_targhe = bc_targhe.Where(s => s.bc_targa.Contains(SearchString));
+            }
+            Targhe = new SelectList(await genreQuery.Distinct().ToListAsync());
+            BC_Targhe = await bc_targhe.ToListAsync();
         }
     }
 }
